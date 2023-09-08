db0b168c9dd094c018c7fac32f041e63fcaa5e57
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2023-09-07 17:18:26 -0700

    Update exercise.cpp

--- exercise.cpp
+++ exercise.cpp
@@ -4,12 +4,5 @@
 
 int main()
 {
-  // Construye un objeto carrito1 a partir de la clase CarritoCompra
-  carrito1.seleccionaArticulos();
-  carrito1.imprimeCarrito();
-  // Construye un objeto carrito2 a partir de la clase CarritoCompra
-  carrito2.seleccionaArticulos();
-  carrito2.imprimeCarrito();
-
   return 0;
 }
